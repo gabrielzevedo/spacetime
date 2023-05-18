91384544aa15ee42344c7dab604325ab1c7402bb
gabrielzevedo <gabrielx86@live.com>
2023-05-18 11:24:13 -0300

    chore: add prisma

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,12 @@
 import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
 
 const app = fastify()
+const prisma = new PrismaClient()
 
-app.get('/', async () => {
-  return { hello: 'world' }
+app.get('/users', async () => {
+  const users = await prisma.user.findMany()
+  return users
 })
 
 app
