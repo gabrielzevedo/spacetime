48205e4ee0ba7da8fd06a290868e1e06abddb421
gabrielzevedo <gabrielx86@live.com>
2023-05-18 22:01:29 -0300

    feat/server: return jwt token in register

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,12 +3,18 @@ import fastify from 'fastify'
 import cors from '@fastify/cors'
 import { memoriesRoutes } from './routes/memories'
 import { authRoutes } from './routes/auth'
+import jwt from '@fastify/jwt'
 
 const app = fastify()
 
 app.register(cors, {
   origin: '*',
 })
+
+app.register(jwt, {
+  secret: process.env.JWT_SECRET || 'spacetime',
+})
+
 app.register(memoriesRoutes)
 app.register(authRoutes)
 
