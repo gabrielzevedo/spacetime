6c8525e3373e92c874dad456dec9ac8c411e5311
gabrielzevedo <gabrielx86@live.com>
2023-05-18 21:48:44 -0300

    feat/server: create auth register route

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,8 @@
+import 'dotenv/config'
 import fastify from 'fastify'
 import cors from '@fastify/cors'
 import { memoriesRoutes } from './routes/memories'
+import { authRoutes } from './routes/auth'
 
 const app = fastify()
 
@@ -8,6 +10,7 @@ app.register(cors, {
   origin: '*',
 })
 app.register(memoriesRoutes)
+app.register(authRoutes)
 
 app
   .listen({
