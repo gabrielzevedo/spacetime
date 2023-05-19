c70dad57fa3798c382d3ba94a1a00308c8629f62
gabrielzevedo <gabrielx86@live.com>
2023-05-19 19:50:24 -0300

    feat/server: upload

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,9 +3,18 @@ import fastify from 'fastify'
 import cors from '@fastify/cors'
 import { memoriesRoutes } from './routes/memories'
 import { authRoutes } from './routes/auth'
+import multipart from '@fastify/multipart'
 import jwt from '@fastify/jwt'
+import { uploadRoutes } from './routes/upload'
+import { resolve } from 'node:path'
 
 const app = fastify()
+
+app.register(multipart)
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+})
 
 app.register(cors, {
   origin: '*',
@@ -15,6 +24,7 @@ app.register(jwt, {
   secret: process.env.JWT_SECRET || 'spacetime',
 })
 
+app.register(uploadRoutes)
 app.register(memoriesRoutes)
 app.register(authRoutes)
 
